c4be7b48620ba84f2ebd18f984f76a6f2c318ac4
TomsGit <ttilli@web.de>
2014-06-06 14:28:11 +0200

    V1

---
+++ plot2.R
@@ -0,0 +1,46 @@
+###
+#using the package
+# sqldf lead to long loading time of the data:
+#library("sqldf")
+#mySql <- "SELECT * from file WHERE Date = '1/2/2007' OR Date = '2/2/2007'"
+#myFile <- "household_power_consumption.txt"
+#myData <- read.csv2.sql(myFile,mySql)
+#
+###############
+# I read first the whole table to find the rows which shall be processed
+# first I find out how muc rows shall can be skipped --> 66637 rows
+# then I find out how much rows have to be processed for the two days mentoined
+#hpc <- read.table("household_power_consumption.txt",
+#                  sep = ";", 
+#                  col.names = colnames(read.table(
+#                  "household_power_consumption.txt",
+#                   nrow = 1, header = TRUE, sep=";")))
+# hpcsub=subset(hpc, Date=="1/2/2007")
+# nrow(hpcsub) -->1440
+# hpcsub=subset(hpc, Date=="2/2/2007")
+# nrow(hpcsub) -->1440
+#########################
+#
+#This leads to the following statement to read only the rows which shall be processed which runs very fast
+#Of course this is a rather brute force approach 
+#
+#
+hpc <- read.table("household_power_consumption.txt",
+                  skip = 66637, nrow = 2880, sep = ";", 
+                  col.names = colnames(read.table(
+                  "household_power_consumption.txt",
+                   nrow = 1, header = TRUE, sep=";")))
+#create new row with date time
+dt= paste(hpc$Date,hpc$Time )
+hpc$DateTime= strptime(dt, "%d/%m/%Y %H:%M:%S")
+
+
+
+#plot2
+plot(hpc$DateTime,hpc$Global_active_power,type="l",main="",xlab="",
+	ylab="Global Active Power (kilowatts)")
+
+
+
+dev.copy(png,file="plot2.png",width=480,height=480,bg = "transparent" )
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,65 @@
+###
+#using the package
+# sqldf lead to long loading time of the data:
+#library("sqldf")
+#mySql <- "SELECT * from file WHERE Date = '1/2/2007' OR Date = '2/2/2007'"
+#myFile <- "household_power_consumption.txt"
+#myData <- read.csv2.sql(myFile,mySql)
+#
+###############
+# I read first the whole table to find the rows which shall be processed
+# first I find out how muc rows shall can be skipped --> 66637 rows
+# then I find out how much rows have to be processed for the two days mentoined
+#hpc <- read.table("household_power_consumption.txt",
+#                  sep = ";", 
+#                  col.names = colnames(read.table(
+#                  "household_power_consumption.txt",
+#                   nrow = 1, header = TRUE, sep=";")))
+# hpcsub=subset(hpc, Date=="1/2/2007")
+# nrow(hpcsub) -->1440
+# hpcsub=subset(hpc, Date=="2/2/2007")
+# nrow(hpcsub) -->1440
+#########################
+#
+#This leads to the following statement to read only the rows which shall be processed which runs very fast
+#Of course this is a rather brute force approach 
+#
+#
+hpc <- read.table("household_power_consumption.txt",
+                  skip = 66637, nrow = 2880, sep = ";", 
+                  col.names = colnames(read.table(
+                  "household_power_consumption.txt",
+                   nrow = 1, header = TRUE, sep=";")))
+#create new row with date time
+dt= paste(hpc$Date,hpc$Time )
+hpc$DateTime= strptime(dt, "%d/%m/%Y %H:%M:%S")
+
+par(mfrow = c(2,2))
+
+#plot.new()
+#--------- plot1 
+plot(hpc$DateTime,hpc$Global_active_power,type="l",main="",xlab="",ylab="Global Active Power")
+
+#------- plot 2
+plot(hpc$DateTime,hpc$Voltage,type="l",main="",xlab="",ylab="Voltage")
+
+#----- plot 3
+plot(hpc$DateTime,hpc$Sub_metering_1,type="l",col="black",main="",xlab="",ylim=c(0,40),ylab="Energy sub metering")
+
+par(new=TRUE)
+plot(hpc$DateTime,hpc$Sub_metering_2,type="l",col="red",main="",xlab="",ylim=c(0,40),ylab="Energy sub metering")
+
+par(new=TRUE)
+plot(hpc$DateTime,hpc$Sub_metering_3,type="l",col="blue",main="",xlab="",ylim=c(0,40),ylab="Energy sub metering")
+legend("topright",lty=1,bty="n",col=c("black","red","blue"),legend=c("Sub metering 1","Sub metering 2","Sub metering 3"))
+
+par(new=FALSE)
+#----------plot 4
+
+plot(hpc$DateTime,hpc$Global_reactive_power,type="l",main="",xlab="",ylab="Global_reactive_power")
+
+
+
+
+dev.copy(png,file="plot4.png",width=480,height=480,bg = "transparent" )
+dev.off()
